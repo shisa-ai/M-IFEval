9511ffbd13874a17bbf840b3bb0cdc64481c3a29
Peter Devine <peter@lightblue-tech.com>
2024-10-02 15:07:02 +0900

    Add full model response generation code

--- get_responses.py
+++ get_responses.py
@@ -1,4 +1,8 @@
 import os
+import argparse
+from glob import glob
+from tqdm.auto import tqdm
+from datasets import load_dataset
 
 class ResponseGenerator:
     def __init__(self, model_name):
@@ -35,7 +39,7 @@ class AnthropicResponseGenerator(ResponseGenerator):
                         ]
                     }
                 ]
-            ).message.content[0].text for input_text in input_texts
+            ).message.content[0].text for input_text in tqdm(input_texts)
         ]
 
 ######## OpenAI ########
@@ -68,7 +72,7 @@ class OpenaiResponseGenerator(ResponseGenerator):
                 frequency_penalty=0,
                 presence_penalty=0,
                 response_format={"type": "text"}
-            ).choices[0].message.content for input_text in input_texts
+            ).choices[0].message.content for input_text in tqdm(input_texts)
         ]
 
 ######## VertexAI ########
@@ -103,7 +107,7 @@ class VertexResponseGenerator(ResponseGenerator):
                 safety_settings=safety_settings
             ).candidates[0].content.parts[0].text
 
-        return [get_vertex_response(input_text) for input_text in input_texts]
+        return [get_vertex_response(input_text) for input_text in tqdm(input_texts)]
         
 
 
@@ -113,7 +117,7 @@ class VllmResponseGenerator(ResponseGenerator):
     def __init__(self, model_name):
         from vllm import LLM, SamplingParams
         self.model_name = model_name
-        self.llm = LLM(model=self.model_name)
+        self.llm = LLM(model=self.model_name, max_model_len=os.environ.get("MAX_MODEL_LEN", 4096))
         self.sampling_params = SamplingParams(temperature=0.0, max_tokens=2048)
 
     def get_response(self, input_texts):
@@ -121,8 +125,63 @@ class VllmResponseGenerator(ResponseGenerator):
             "role": "user",
             "content": input_text
         }] for input_text in input_texts]
-        
+
         outputs = self.llm.chat(input_conversations,
                    sampling_params=self.sampling_params,
                    use_tqdm=True)
         return [output.outputs[0].text for output in outputs]
+
+######## Main ########
+
+SUPPORTED_MODELS = {
+    'gpt-4o-mini-2024-07-18': 'gpt',
+    'gpt-4o-2024-08-06': 'gpt',
+    'o1-preview-2024-09-12': 'gpt',
+    'o1-mini-2024-09-12': 'gpt',
+    'claude-3-haiku-20240307': 'claude',
+    'claude-3-5-sonnet-20240620': 'claude',
+    'claude-3-opus-20240229': 'claude',
+    'gemini-1.5-pro-002': 'gemini',
+    'CohereForAI/c4ai-command-r-plus-4bit': 'vllm',
+    'CohereForAI/c4ai-command-r-v01-4bit': 'vllm',
+    'CohereForAI/aya-23-8B': 'vllm',
+    'Qwen/Qwen2.5-0.5B-Instruct-GPTQ-Int4': 'vllm',
+    'Qwen/Qwen2.5-1.5B-Instruct-GPTQ-Int4': 'vllm',
+    'Qwen/Qwen2.5-3B-Instruct-GPTQ-Int4': 'vllm',
+    'Qwen/Qwen2.5-7B-Instruct-GPTQ-Int4': 'vllm',
+    'Qwen/Qwen2.5-14B-Instruct-GPTQ-Int4': 'vllm',
+    'Qwen/Qwen2.5-32B-Instruct-GPTQ-Int4': 'vllm',
+    'Qwen/Qwen2.5-72B-Instruct-GPTQ-Int4': 'vllm',
+    'hugging-quants/Meta-Llama-3.1-70B-Instruct-AWQ-INT4': 'vllm',
+    'hugging-quants/Meta-Llama-3.1-8B-Instruct-AWQ-INT4': 'vllm',
+    'mistralai/Mistral-7B-Instruct-v0.3': 'vllm'
+}
+
+MODEL_CLASS_DICT = {
+    "gpt": OpenaiResponseGenerator,
+    "claude": AnthropicResponseGenerator,
+    "gemini": VertexResponseGenerator,
+    "vllm": VllmResponseGenerator,
+}
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model", type=str, required=True)
+    args = parser.parse_args()
+
+    model_name = args.model
+
+    assert model_name in SUPPORTED_MODELS, f"Model {model_name} not supported"
+
+    paths = sorted(glob("./data/*_input_data.jsonl"))
+
+    model_class = MODEL_CLASS_DICT[SUPPORTED_MODELS[model_name]]
+    response_generator = model_class(model_name)
+
+    for path in paths:
+        print(path + " - " + model_name)
+        ds = load_dataset("json", data_files={"train": path}, split="train")
+        ds = ds.add_column("response", response_generator.get_response(ds["prompt"]))
+        ds.to_json(
+            path[:-10] + "response_data_" + model_name + ".jsonl"
+        )
